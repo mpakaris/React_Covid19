424b016c6c23faeb9109a952a9e800efbf926742
Nikos Mpakaris <nikos.mpakaris@gmail.com>
2020-07-19 18:26:59 +0200

    Updates

---
+++ precache-manifest.ae6d7cb0eb7aa444b13740d2ffed5698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d185aa49a30a1e91e9440aee8e023c7",
+    "url": "/React_Covid19/index.html"
+  },
+  {
+    "revision": "608f1e4099641253e27d",
+    "url": "/React_Covid19/static/css/main.8218fec5.chunk.css"
+  },
+  {
+    "revision": "120acf54383dca00f1ba",
+    "url": "/React_Covid19/static/js/2.b48adf4c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/React_Covid19/static/js/2.b48adf4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608f1e4099641253e27d",
+    "url": "/React_Covid19/static/js/main.f02db600.chunk.js"
+  },
+  {
+    "revision": "bb1c34afe71436233f7e",
+    "url": "/React_Covid19/static/js/runtime-main.56cd2bfc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/React_Covid19/static/media/image.d7265326.png"
+  }
+]);
